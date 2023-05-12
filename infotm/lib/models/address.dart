840e2ffd26fab24130718df2225c79ea6177ad2c
stefanfaur <stefan.faur@student.upt.ro>
2023-05-12 22:18:45 +0300

    Added Pin and Address models.

---
+++ infotm/lib/models/address.dart
@@ -0,0 +1,23 @@
+class Address {
+  late String street;
+  late String city;
+  late String region;
+  late String country;
+
+  Address(this.street, this.city, this.region, this.country);
+
+  Address.fromJson(Map<String, dynamic> json) {
+    street = json['street'];
+    city = json['city'];
+    region = json['region'];
+    country = json['country'];
+  }
+
+  Map<String, dynamic> toJson() =>
+      {'street': street, 'city': city, 'region': region, 'country': country};
+
+  @override
+  String toString() {
+    return '$street, $city, $region, $country';
+  }
+}
